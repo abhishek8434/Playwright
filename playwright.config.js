93950ee4f2928ed41c8e7198aea35d35a1562c01
abhishek8434 <61007819+abhishek8434@users.noreply.github.com>
2024-09-19 23:57:11 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,6 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
-  ['html', { outputFolder: 'playwright-report', open: 'never' }],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

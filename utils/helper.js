5e4db8daaa3fcc0d9add37b0d86155d324677805
priyansh70 <priyansh70890@gmail.com>
2024-12-24 12:03:16 +0530

    Update Helper File, added Login Functionality in helper

--- utils/helper.js
+++ utils/helper.js
@@ -1,14 +1,33 @@
+import { faker } from '@faker-js/faker';
+import * as dotenv from "dotenv";
 const { expect } = require('@playwright/test');
-// const { locators } = require('');
 const { locators } = require("../constants/locators")
 
-// Helper function for login
-export async function login(page, email, password) {
-  await page.goto('/');
+dotenv.config();
+
+const LOGIN_URL = process.env.LOGIN_URL;
+const LOGIN_EMAIL = process.env.LOGIN_EMAIL1;
+const LOGIN_PASSWORD = process.env.LOGIN_PASSWORD1;
+const LOGIN_PASSWORD_SECONDARY = process.env.LOGIN_PASSWORD_SECONDARY;
+
+
+// Initial Phase 
+export async function initiateScript(page) {
+  // Navigate to the login page
+  await page.goto(LOGIN_URL);
+
+  // Click on LOGIN link and complete login steps
   await page.getByRole('link', { name: 'LOGIN' }).click();
-  await page.getByLabel('* Email Address').fill(email);
-  await page.getByLabel('* Password').fill(password);
+  await page.getByRole('textbox', { name: 'Enter your Password' }).fill(LOGIN_PASSWORD_SECONDARY);
   await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  // Secondary login if necessary
+  await page.waitForSelector('text=Login');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByLabel('* Email Address').type(LOGIN_EMAIL, { delay: 100 });
+  await page.getByLabel('* Password').type(LOGIN_PASSWORD, { delay: 100 });
+  await page.getByLabel('* Password').press('Enter');
   await page.getByRole('button', { name: 'Continue' }).click();
 }
 
@@ -51,4 +70,22 @@ export const livingfatherhusband = ["living", "deceased"];
 
 export const livingfatherwife = ["living", "deceased"];
 
+// Nigerian phone numbers start with prefixes such as 080, 081, 090, 070 followed by 7 digits
+// Generate a phone number in the format +234XXXXXXXXXX
 
+export function generateNigerianPhoneNumber() {
+  // Array of valid prefixes
+  const prefixes = ['803', '806', '813']; // Common prefixes for Nigerian networks
+
+  // Randomly select a prefix from the array
+  const randomPrefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+
+  const randomPhoneBody = faker.string.numeric(7); // Generate a 7-digit number
+  return `+234-${randomPrefix}-${randomPhoneBody.slice(0, 4)} ${randomPhoneBody.slice(4)}`; // Format the number
+}
+
+export function getFullName(gender) {
+  let firstName = faker.person.firstName(gender);
+  let lastName = faker.person.firstName(gender);
+  return (firstName + " " + lastName);
+}
